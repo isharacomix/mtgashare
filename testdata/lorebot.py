38cde1e157638b305129cd15c095ed83128b3113
isharacomix <ishara@isharacomix.org>
2021-05-28 20:29:46 -0400

    experiment

---
+++ testdata/lorebot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+import os, sys
+import json, wget
+import requests
+
+
+
+
+def lore():
+    result = ""
+    result += """<?xml version="1.0" encoding="utf-8"?>
+<?xml-stylesheet type="text/xsl" href="https://magic.wizards.com/sites/all/themes/wiz_mtg/xml/rss.xsl"?>
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0" xml:base="https://mtgesports.com"><channel><title>MTG Esports</title><link>https://mtgesports.com</link><description/><language>en</language>"""
+
+    data = requests.get("https://pathfinderwiki.com/wiki/Special:Random/Main")
+    
+    name = data.url.split('/')[-1].replace('_', ' ')
+    summary = data.text.split('role="article"')[1].split('<p>')[1].split('</p>')[0]
+    summary = summary.replace('[', '(').replace(']',')')
+
+
+    result += "<item>"
+    result += "<title>"+name+"</title>"
+    result += "<link>"+data.url+"</link>"
+    result += "<guid>"+data.url+"</guid>"
+    result += "<description><![CDATA["
+    result += summary
+    result += "]]></description>"
+    result += "</item>"
+
+    result += """</channel></rss>"""
+    open('../lore.rss', "w").write(result)
+
+
+if __name__ == '__main__':
+    lore()
